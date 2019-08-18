e13cf3b0045c4e52d91bd7551883b8a4c6567701
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 15:33:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,6 +50,12 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length 
+    return false if source_array[i] == false || source_array[i] == nil 
+    i += 1 
+  end
+  true 
 end 
 
 def reduce_to_any_true(source_array)
