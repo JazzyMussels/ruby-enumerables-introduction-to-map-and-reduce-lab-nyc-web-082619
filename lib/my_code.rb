10d14f7f8695a74bc145e3ca4e58590a53309c2c
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 15:23:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,6 +20,13 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  new = [] 
+   i = 0 
+  while i < source_array.length 
+    new << source_array[i] * 2 
+    i += 1 
+  end 
+  new 
 end
 
 def  map_to_square(source_array)
